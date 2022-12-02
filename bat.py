b697e3f45db19acd4b1cbaeba617c69beeff0ca6
BananenKraft <mikakoerner@outlook.de>
2022-12-02 08:51:57 -0500

    Make Player bat movable, fix other issues

--- bat.py
+++ bat.py
@@ -3,8 +3,18 @@ import math
 
 class Bat:
 
-    def __init__(self, screen, startX, startY, width=30, heigth=70):
-        self.startX = startX-(math.ceil(startX/2))
-        self.startY = startY-(math.ceil(startY/2))
+    def __init__(self, screen, startX, startY, width=30, height=70):
+        self.startX = startX-(math.ceil(width/2))
+        self.startY = startY-(math.ceil(height/2))
         self.screen = screen
-        self.rect = pygame.draw.rect(self.screen, (255,255,255), pygame.Rect(startX, startY, width, heigth))
+        self.width = width
+        self.height = height
+        self.rect = self.drawCurrent()
+
+    def drawCurrent(self):
+        self.rect = pygame.draw.rect(self.screen, (255,255,255), pygame.Rect(self.startX, self.startY, self.width, self.height))
+        return self.rect
+
+    def move(self,y):
+        self.startY -= y
+        return self.rect
--- pong.py
+++ pong.py
@@ -2,7 +2,6 @@ import sys
 import pygame
 from settings import Settings
 from bat import Bat
-from listener import Listener
 
 def run_game():
 
@@ -19,17 +18,25 @@ def run_game():
     screen = pygame.display.set_mode((int(settings.screen_width), int(settings.screen_height)))
     pygame.display.set_caption("Pong")
     background_color = (30,30,30)
-    screen.fill(background_color)
-    mainlistener = Listener()
-    bat = Bat(screen, 20,20)
-    enemy_bat = Bat(screen, settings.screen_width-50, 20)
+    bat = Bat(screen, 30,50)
+    enemy_bat = Bat(screen, screen.get_width()-30, 50)
 
     #Main game loop
     while True:
 
-        mainlistener.checkEvents()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_DOWN:
+                    bat.move(-10)
+                if event.key == pygame.K_UP:
+                    bat.move(10)
+                    print("Done")
+                    
+        screen.fill(background_color)
+        bat.drawCurrent()
+        enemy_bat.drawCurrent()
         pygame.display.flip()
 
 run_game()
-    
-    
